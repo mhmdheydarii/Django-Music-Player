a0d6bb0189e0243eef7c4e2b85d8e13b05233f61
mhmdheydarii <heidarimohammadhosin@gmail.com>
2026-06-09 03:30:55 -0700

    clean signal codes

--- music/signals.py
+++ music/signals.py
@@ -5,19 +5,11 @@ from django.core.cache import cache
 from .models import Category, Singer
 
 # Clear singer cache on update
-@receiver(post_save, sender=Singer)
-def clear_singer_cache_on_save(sender, instance, **kwargs):
-    cache.delete("homepage_singers")
-
-@receiver(post_delete, sender=Singer)
-def clear_singer_cache_on_delete(sender, instance, **kwargs):
+@receiver([post_save, post_delete], sender=Singer)
+def clear_singer_cache(sender, instance, **kwargs):
     cache.delete("homepage_singers")
 
 # Clear category cache on update
-@receiver(post_save, sender=Category)
-def clear_category_cache_on_save(sender, instance, **kwargs):
-    cache.delete("homepage_categories")
-
-@receiver(post_delete, sender=Category)
-def clear_category_cache_on_delete(sender, instance, **kwargs):
+@receiver([post_save, post_delete], sender=Category)
+def clear_category_cache(sender, instance, **kwargs):
     cache.delete("homepage_categories")
